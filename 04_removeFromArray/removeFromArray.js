a30c4dae780fba3290237ae9d31a11c2120769d2
JordanLindley <jlindley94@gmail.com>
2022-09-27 23:04:18 -0400

    Complete exercises 4 - 5
    These gave me some trouble. Needed to peek at the solution for 5.
    I had everything right except for the conditional to check if the
    entered parameters were numbers. The overall order of the logic was
    out of place.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...itemsRemoved) {
+  // Identify index(es) of itemsRemoved
+  // Array.splice() the identified indexes
+  itemsRemoved.forEach((item) => {
+    const index = array.indexOf(item);
+    if (index > -1) {
+      array.splice(index, 1);
+    }
+  });
+  return array;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  // Create a variable to hold the final sum.
+  let finalSum = 0;
+  // if num1 is greater than num2, swap them.
+  if (num1 > num2) {
+    const temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
 
+  // Create conditional where function returns 'ERROR' with negative or NaN numbers.
+  if (num1 < 0 || num2 < 0) return 'ERROR';
+  if (!Number.isInteger(num1) || !Number.isInteger(num2)) return 'ERROR' 
+
+  // Loop through given numbers.
+  for (i = num1; i <= num2; i++) {
+        // add the looped number to the finalSum.
+        finalSum += i;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
