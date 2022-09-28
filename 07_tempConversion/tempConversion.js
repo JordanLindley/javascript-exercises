d8f301d428d4548b289b33d0882bf65260d10700
JordanLindley <jlindley94@gmail.com>
2022-09-27 23:26:31 -0400

    Complete exercise 7: tempConversion
    Tests passed for tempConverstion. Googled Math.round() for the right way
    to round to nearest decimal in F and C conversion formulas. Wrapped up
    Part 4 of Fundamentals with Odin Project.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempFahrenheit) {
+  tempC = ((tempFahrenheit - 32) * 5) / 9;
+  return Math.round(tempC * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempCelsius) {
+  tempF = (tempCelsius * (9/5)) + 32;
+  return Math.round(tempF * 10) / 10;
 };
 
 // Do not edit below this line
