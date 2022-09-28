7583d3e720c81bb19807651123be74504f8aa411
JordanLindley <jlindley94@gmail.com>
2022-09-27 21:56:01 -0400

    Compelete Excercises 1 through 3
    And finally learned the true pain of nitpicking a javascript program
    for forty minutes because the test kept failing only to find that I
    had added a bug in the test itself, and that the program was fine all
    along.
    
    I feel like a real programmer.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  const stringToArray = string.split('');
+  const reverseString = stringToArray.reverse().join('');
+  return reverseString;
 };
 
 // Do not edit below this line
