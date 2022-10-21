d617d19b497517f01695572743be5c85bd6eb138
JordanLindley <jlindley94@gmail.com>
2022-10-21 10:21:53 -0400

    completed exercise 09: palidromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  const reg = /[\W_]/g;
+  string = string.toLowerCase().replace(reg, '');
+// USING ARRAY METHODS
+  const reversed = string.split('').reverse().join('');
+  if (reversed !== string) return false;
 
+  return true;
+
+// USING A FOR LOOP 
+  // const opposite = string.length - 1
+  // for (let i = 0; i < opposite - i; i++) {
+  //   if (string[i] !== string[opposite - i]) {
+  //     return false;
+  //   }
+  // }
+
+  // return true;
 };
 
 // Do not edit below this line
